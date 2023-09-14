05ec24106d25be2c0fa80f9a0fcca065c7387e7e
Brice JUHEL <juhel.brice@gmail.com>
2023-09-14 14:25:42 +0200

    creation composant NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
 import Home from './pages/Home';
+import { Club } from './pages/Club';
+import { Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
-      <Home/>
+      <NavBar/>
+      <Routes>        
+        <Route path='/' element={ <Home/>}/>
+        <Route path='/club' element={ <Club/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Club.tsx
@@ -0,0 +1,7 @@
+export const Club = () => {
+    return (
+    <div>
+        <h3>Organisation du club</h3>
+    </div>
+    )
+}
