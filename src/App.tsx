8a34f8bfc1c1539499df3f702ccff680a22252ff
Brice JUHEL <juhel.brice@gmail.com>
2023-09-14 16:39:36 +0200

    MAJ page Home

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import { Club } from './pages/Club';
 import { Routes, Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={ <Home/>}/>
         <Route path='/club' element={ <Club/>}/>
       </Routes>
+      <Footer/>
     </div>
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,20 +5,19 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 const navigation = [
   { name: 'Le club', href: '/club' },
   { name: 'Résultats', href: '#' },
-  { name: 'Evenements', href: '#' },
+  { name: 'Évenements', href: '#' },
   { name: 'Contact', href: '#' },
 ]
 
-export default function Example() {
+export default function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
+      <header className="absolute inset-x-0 top-0 z-50 2xl:w-2/3 2xl:mx-auto">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
               <img
                 className="lg:h-24 h-16 w-auto"
                 src="/images/logo.png"
