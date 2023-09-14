ea5426a5c4c69762f5e18984de7ab256f73c1d36
Brice JUHEL <juhel.brice@gmail.com>
2023-09-14 12:39:49 +0200

    mise en place projet

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+export const About = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">
+            Hello world!
+            </h1>
+            <p>Test</p>
+        </div>
+    )
+}
---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,12 @@
+export const SocialLinks = () => {
+    return (
+        <section>
+            <h3>Mes Réseaux Sociaux</h3>
+        <ul>
+            <li><a href="#">Youtube</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Discord</a></li>
+        </ul>
+        </section>
+    )
+}
